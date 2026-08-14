7cf846cecccbb7e2320cc72947931635e95b4968
YamelPinta <pintayamel@gmail.com>
2026-08-13 23:58:14 -0300

    Add files via upload
    v25

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_ESTATICO = 'gimnasio-estatico-v24'; // <-- ACORDATE DE CAMBIAR EL NÚMERO 
-const CACHE_DINAMICO = 'gimnasio-dinamico-v24'; // <-- PARA FORZAR LA ACTUALIZACIÓN
+const CACHE_ESTATICO = 'gimnasio-estatico-v25'; // <-- ACORDATE DE CAMBIAR EL NÚMERO 
+const CACHE_DINAMICO = 'gimnasio-dinamico-v25'; // <-- PARA FORZAR LA ACTUALIZACIÓN
 
 const ASSETS_CORE = [
     '/',
